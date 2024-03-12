bf32312503be7a143291e6dcd042c894cbd3a852
Polaris1491319352 <1491319352@qq.com>
2024-03-12 18:59:09 +0800

    test

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value="/hello")
+    public String index(ModelMap map){
+        map.addAttribute("name","zhangsan");
+        return "Hello116";
+    }
+}
